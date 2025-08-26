81b2137685c09c999b6c5d218f342e1652d4514a
박용현 <pyh5523@gmail.com>
2025-08-26 20:16:39 +0900

    feat: update home route to render index.html with computer name feat: add index.html template for the home page

--- repo_work/app.py
+++ repo_work/app.py
@@ -1,16 +1,25 @@
-#Flask class 가져옴
+# Flask class 가져옴
 from flask import Flask, render_template
+import socket  # socket 모듈 import 추가
 
 # Flask 애플리케이션 생성(인스턴스)
 app = Flask(__name__)
-
-#라우트 데코레이터 :  URL 경로 "/" 요청 처리
+# route : root
+# 라우트 데코레이터 : URL 경로 "/" 요청 처리
 # 브라우저에서 http://localhost:8080/ 접속 시 아래 함수가 실행됨
 @app.route('/')
-def hello_world():
-    return 'Hello, DevOps!'
-#route : menu
-#menu.html 파일을 렌더링함
+def home():  # 함수명을 hello_world에서 home으로 변경
+    # 컴퓨터 이름(호스트네임) 설정
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    
+    # index.html 템플릿을 렌더링하며 computername 변수 전달
+    return render_template('index.html', computername=hostname)
+
+# route : menu
+# menu.html 파일을 렌더링함
 # render_template : Render a template by name with the given context.
 @app.route('/menu')
 def menu():
@@ -20,7 +29,7 @@ if __name__ == '__main__':
     # Flask 웹 서버 시작
     # host="0.0.0.0": 모든 네트워크 인터페이스에서 접속 허용
     # port=8080: 8080 포트에서 서버 실행
-    app.run(host="0.0.0.0", port=8080)
-    #실행중 출력 
+    # debug=True 옵션 추가
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    # 실행중 출력 
     print("Flask server is running on http://localhost:8080/")
-
