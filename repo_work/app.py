aa565f38dd110cfe354030bed5f4d9c638fd1914
pista <43775386+SteelCrab@users.noreply.github.com>
2025-08-27 11:31:17 +0900

    Update app.py

--- repo_work/app.py
+++ repo_work/app.py
@@ -25,6 +25,11 @@ def home():  # 함수명을 hello_world에서 home으로 변경
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+# route: test2
+# test2.html 렌더링함 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
 
 if __name__ == '__main__':
     # Flask 웹 서버 시작
